6f0e7e732a63b5e35fc561b67fb45465def23419
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-01-05 23:52:16 +0000

    Created log class

---
+++ customSailSkins/Log.cs
@@ -0,0 +1,9 @@
+﻿using BWModLoader;
+
+namespace customSailSkins
+{
+    internal static class Log
+    {
+        static readonly public ModLogger logger = new ModLogger("[FlagReplacement]", null);
+    }
+}
