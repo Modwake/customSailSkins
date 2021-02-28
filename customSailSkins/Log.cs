fe6da455c8788631ef97ee7e65e230badf899ba1
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-02-28 12:01:13 +0000

    Fixed output name

--- customSailSkins/Log.cs
+++ customSailSkins/Log.cs
@@ -4,6 +4,6 @@ namespace customSailSkins
 {
     internal static class Log
     {
-        static readonly public ModLogger logger = new ModLogger("[FlagReplacement]", null);
+        static readonly public ModLogger logger = new ModLogger("[customSailSkins]", null);
     }
 }
